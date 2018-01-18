9e46f72700c9a567b109c8f136f7bf37fbf3295a
Elis Dervishi <elis.derv@gmail.com>
2018-01-18 12:41:58 -0500

    init

--- client/components/main.js
+++ client/components/main.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
-import {logout} from '../store'
 
 /**
  * COMPONENT
@@ -11,23 +10,15 @@ import {logout} from '../store'
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props
+  const {children, handleClick} = props
 
   return (
     <div>
       <h1>BOILERMAKER</h1>
       <nav>
         {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
+            <div>
               <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
             </div>
         }
       </nav>
--- client/routes.js
+++ client/routes.js
@@ -1,10 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Route, Switch, Router} from 'react-router-dom'
-import PropTypes from 'prop-types'
-import history from './history'
-import {Main, Login, Signup, UserHome} from './components'
-import {me} from './store'
+import {Main} from './components'
 
 /**
  * COMPONENT
@@ -15,7 +12,6 @@ class Routes extends Component {
   }
 
   render () {
-    const {isLoggedIn} = this.props
 
     return (
       <Router history={history}>
@@ -40,16 +36,6 @@ class Routes extends Component {
   }
 }
 
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
 
 const mapDispatch = (dispatch) => {
   return {
@@ -61,10 +47,3 @@ const mapDispatch = (dispatch) => {
 
 export default connect(mapState, mapDispatch)(Routes)
 
-/**
- * PROP TYPES
- */
-Routes.propTypes = {
-  loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
